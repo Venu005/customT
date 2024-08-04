2c16fdc70c7634e0a2bd5d8ab1e996bd7f93bf3d
Venu005 <venusaiyalamanchili@gmail.com>
2024-08-04 22:55:51 +0530

    feat: landing page done

--- src/fonts/monaSans.ts
+++ src/fonts/monaSans.ts
@@ -3,7 +3,7 @@ import localFont from "next/font/local";
 export const monaSans = localFont({
   src: [
     {
-      path: "./Mona-Sans-BlackNarrow.woff2",
+      path: "./Mona-Sans-Black.woff2",
       weight: "900",
       style: "normal",
     },
@@ -32,16 +32,7 @@ export const monaSans = localFont({
       weight: "300",
       style: "italic",
     },
-    {
-      path: "./Mona-Sans-MediumNarrow.woff2",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "./Mona-Sans-MediumNarrowItalic.woff2",
-      weight: "500",
-      style: "italic",
-    },
+
     {
       path: "./Mona-Sans-SemiBoldNarrow.woff2",
       weight: "600",
@@ -70,6 +61,16 @@ export const monaSans = localFont({
     {
       path: "./Mona-Sans-ExtraBoldWide.woff2",
       weight: "800",
+      style: "normal",
+    },
+    {
+      path: "./Mona-Sans-Medium.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./Mona-Sans-RegularWide.woff2",
+      weight: "500",
       style: "normal",
     },
   ],
